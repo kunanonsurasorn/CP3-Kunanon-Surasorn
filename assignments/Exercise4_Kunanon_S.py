2584c19088444b8225f6848394bb6bca869ebf0b
Kunanon Surasorn <kunanonsurasorn@gmail.com>
2026-01-04 16:13:20 +0700

    Update Exercise4_Kunanon_S.py

--- assignments/Exercise4_Kunanon_S.py
+++ assignments/Exercise4_Kunanon_S.py
@@ -1,1 +1,14 @@
-print("Hello World")
+#FE is Foundation English.
+FE_score = 72.50
+#GB is General Business.
+GB_score = 82.25
+#ICS is Introduction to Computer Systems.
+ICS_score = 76.75
+#CP is Computer Programming.
+CP_score = 90.00
+print("----------------Your Score----------------")
+print("Foundation English: ", FE_score)
+print("General Business: ", GB_score)
+print("Introduction to Computer Systems: ", ICS_score)
+print("Computer Programming: ", CP_score)
+print("------------------------------------------")
