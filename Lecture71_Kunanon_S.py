fb247d891f25dfb809da050f048587afbf9535ce
Kunanon Surasorn <kunanonsurasorn@gmail.com>
2026-01-21 18:18:47 +0700

    Create Lecture71_Kunanon_S.py

---
+++ Lecture71_Kunanon_S.py
@@ -0,0 +1,19 @@
+menuList = []
+priceList = []
+calculatePrice = priceList
+
+def showBill():
+    print("---- My Food ----")
+    for number in range(len(menuList)):
+        print(menuList[number],priceList[number])
+    print("Total :",sum(calculatePrice))
+
+while True:
+    menuName = input("Please Enter Menu : ")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = int(input("Price : "))
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+showBill()
