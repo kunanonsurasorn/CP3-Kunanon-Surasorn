4e8ae44291a3c0a2e3383e121671e6c0a8e75341
Kunanon Surasorn <kunanonsurasorn@gmail.com>
2026-02-04 17:25:08 +0700

    Lecture114

--- assignments/Lecture114_Kunanon_S.py
+++ assignments/Lecture114_Kunanon_S.py
@@ -2,42 +2,47 @@ from forex_python.converter import CurrencyRates
 from datetime import datetime,timedelta
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from tkinter import * 
+import tkinter as tk
 
 currency_rates = CurrencyRates()
-present_day = datetime.today()
-present_day_format = present_day.strftime("%d/%m/%Y")
-list_currency = ["THB","USD","EUR","GBP","CNY","JPY"]
+today = datetime.today()
+today_str = today.strftime("%d/%m/%Y")
+currency_list = ["THB","USD","EUR","GBP","CNY","JPY"]
 
 def convert_button_click():
     try:
         money_amount_input = float(money_input.get())
     except ValueError:
-        label_result4.configure(text="Invalid number")
+        converted_currency_result.configure(text="Invalid number")
         return
+    
     original_currency = original_currency_var.get()
     converted_currency = converted_currency_var.get()
-    rate = currency_rates.get_rate(original_currency, converted_currency, present_day)
+
+    rate = currency_rates.get_rate(original_currency, converted_currency, today)
     money_value_output = money_amount_input*rate
     money_value_output = round(money_value_output,2)
-    label_result1.configure(text=original_currency)
-    label_result2.configure(text=money_amount_input)
-    label_result3.configure(text=converted_currency)
-    label_result4.configure(text=money_value_output)
+
+    original_currency_label.configure(text=original_currency)
+    original_currency_result.configure(text=money_amount_input)
+    converted_currency_label.configure(text=converted_currency)
+    converted_currency_result.configure(text=money_value_output)
 
 def currency_history(original_currency,converted_currency,days=7):
-    date_currency_value = []
+    rate_history = []
+
     for i in range(days):
-        date = present_day - timedelta(days=i)
-        date = date.date()
-        rate = currency_rates.get_rate(original_currency, converted_currency, date)
+        current_date = today - timedelta(days=i)
+        current_date = current_date.date()
+        rate = currency_rates.get_rate(original_currency, converted_currency, current_date)
         rate = round(rate, 2)
-        date_currency_value.append((date, rate))
-    return list(reversed(date_currency_value))
+        rate_history.append((current_date, rate))
+
+    return list(reversed(rate_history))
     
-def currency_graph(date_currency_value,original_currency,converted_currency):
-    x = [item[0] for item in date_currency_value]
-    y = [item[1] for item in date_currency_value]
+def currency_graph(rate_history,original_currency,converted_currency):
+    x = [item[0] for item in rate_history]
+    y = [item[1] for item in rate_history]
     
     plt.figure()
     plt.plot(x, y)
@@ -60,97 +65,97 @@ def compare_button_click():
         return round(((rate_today - past_rate) / past_rate) * 100, 2)
     
     #present day sequence is -1.
-    one_day_ago = -2
-    seven_days_ago = -8
+    one_day_ago = len(history) - 2
+    seven_days_ago = len(history) - 8
     thirty_days_ago = 0
 
-    label_result6.configure(text=f"{diff_from_index(one_day_ago)} %")
-    label_result7.configure(text=f"{diff_from_index(seven_days_ago)} %")
-    label_result8.configure(text=f"{diff_from_index(thirty_days_ago)} %")
+    date_before_1day_difference_output.configure(text=f"{diff_from_index(one_day_ago)} %")
+    date_before_7day_difference_output.configure(text=f"{diff_from_index(seven_days_ago)} %")
+    date_before_30day_difference_output.configure(text=f"{diff_from_index(thirty_days_ago)} %")
     
     currency_graph(history, original_currency, converted_currency)
 
-main_window = Tk()
+main_window = tk.Tk()
 
-title_label = Label(main_window, text = "Foreign Exchange Currency")
+title_label = tk.Label(main_window, text = "Foreign Exchange Currency")
 title_label.grid(row = 0, column = 1)
 
-original_currency_label = Label(main_window, text = "Original Currency")
-original_currency_label.grid(row = 1, column = 0)
+original_currency_title_label = tk.Label(main_window, text = "Original Currency")
+original_currency_title_label.grid(row = 1, column = 0)
 
-original_currency_var = StringVar()
-converted_currency_var = StringVar()
+original_currency_var = tk.StringVar()
+converted_currency_var = tk.StringVar()
 
 original_currency_var.set("USD")
 converted_currency_var.set("THB")
 
-original_currency_menu = OptionMenu(main_window,original_currency_var,*list_currency)
+original_currency_menu = tk.OptionMenu(main_window,original_currency_var,*currency_list)
 original_currency_menu.grid(row=1, column=1)
 
-converted_currency_menu = OptionMenu(main_window,converted_currency_var,*list_currency)
+converted_currency_menu = tk.OptionMenu(main_window,converted_currency_var,*currency_list)
 converted_currency_menu.grid(row=2, column=1)
 
-converted_currency_label = Label(main_window, text = "Converted Currency")
-converted_currency_label.grid(row = 2, column = 0)
+converted_currency_title_label = tk.Label(main_window, text = "Converted Currency")
+converted_currency_title_label.grid(row = 2, column = 0)
 
-money_input_label = Label(main_window, text = "Input Money")
+money_input_label = tk.Label(main_window, text = "Input Money")
 money_input_label.grid(row = 3, column = 0)
 
-money_input = (Entry(main_window))
+money_input = (tk.Entry(main_window))
 money_input.grid(row = 3, column = 1)
 
-convert_button = Button(main_window,text="Convert Currency",command=convert_button_click)
+convert_button = tk.Button(main_window,text="Convert Currency",command=convert_button_click)
 convert_button.grid(row=5, column=0)
 
-compare_button = Button(main_window,text="Compare & Graph",command=compare_button_click)
+compare_button = tk.Button(main_window,text="Compare & Graph",command=compare_button_click)
 compare_button.grid(row=5, column=1)
 
-label_result1 = Label(main_window, text = "Original Currency")
-label_result1.grid(row = 6, column = 0)
+original_currency_label = tk.Label(main_window, text = "Original Currency")
+original_currency_label.grid(row = 6, column = 0)
 
-label_result2 = Label(main_window, text = "Original Currency Output")
-label_result2.grid(row = 6, column = 1)
+original_currency_result = tk.Label(main_window, text = "Original Currency Output")
+original_currency_result.grid(row = 6, column = 1)
 
-label_result3 = Label(main_window, text = "Converted Currency")
-label_result3.grid(row = 7, column = 0)
+converted_currency_label = tk.Label(main_window, text = "Converted Currency")
+converted_currency_label.grid(row = 7, column = 0)
 
-label_result4 = Label(main_window, text = "Converted Currency Output")
-label_result4.grid(row = 7, column = 1)
+converted_currency_result = tk.Label(main_window, text = "Converted Currency Output")
+converted_currency_result.grid(row = 7, column = 1)
 
-date_label = Label(main_window, text = "Date")
+date_label = tk.Label(main_window, text = "Date")
 date_label.grid(row = 4, column = 0)
 
-date_output0 = Label(main_window, text = f"{present_day_format}")
-date_output0.grid(row = 4, column = 1)
+today_output = tk.Label(main_window, text = f"{today_str}")
+today_output.grid(row = 4, column = 1)
 
-label_result5 = Label(main_window, text = "Difference")
-label_result5.grid(row = 1, column = 2)
+difference_label = tk.Label(main_window, text = "Difference")
+difference_label.grid(row = 1, column = 2)
 
-date_before_oneday = present_day - timedelta(days=1)
-date_before_oneday = date_before_oneday.date()
-date_before_oneday_format = date_before_oneday.strftime("%d/%m/%Y")
-date_output1 = Label(main_window, text = f"{date_before_oneday_format}")
-date_output1.grid(row = 2, column = 2)
+date_before_1day = today - timedelta(days=1)
+date_before_1day = date_before_1day.date()
+date_before_1day_format = date_before_1day.strftime("%d/%m/%Y")
+date_before_1day_output = tk.Label(main_window, text = f"{date_before_1day_format}")
+date_before_1day_output.grid(row = 2, column = 2)
 
-label_result6 = Label(main_window, text = 0)
-label_result6.grid(row = 3, column = 2)
+date_before_1day_difference_output = tk.Label(main_window, text = 0)
+date_before_1day_difference_output.grid(row = 3, column = 2)
 
-date_before_twodays = present_day - timedelta(days=7)
-date_before_twodays = date_before_twodays.date()
-date_before_twodays_format = date_before_twodays.strftime("%d/%m/%Y")
-date_output2 = Label(main_window, text = f"{date_before_twodays_format}")
-date_output2.grid(row = 4, column = 2)
+date_before_7days = today - timedelta(days=7)
+date_before_7days = date_before_7days.date()
+date_before_7days_format = date_before_7days.strftime("%d/%m/%Y")
+date_before_7days_output = tk.Label(main_window, text = f"{date_before_7days_format}")
+date_before_7days_output.grid(row = 4, column = 2)
 
-label_result7 = Label(main_window, text = 0)
-label_result7.grid(row = 5, column = 2)
+date_before_7day_difference_output = tk.Label(main_window, text = 0)
+date_before_7day_difference_output.grid(row = 5, column = 2)
 
-date_before_threedays = present_day - timedelta(days=30)
-date_before_threedays = date_before_threedays.date()
-date_before_threedays_format = date_before_threedays.strftime("%d/%m/%Y")
-date_output3 = Label(main_window, text = f"{date_before_threedays_format}")
-date_output3.grid(row = 6, column = 2)
+date_before_30days = today - timedelta(days=30)
+date_before_30days = date_before_30days.date()
+date_before_30days_format = date_before_30days.strftime("%d/%m/%Y")
+date_before_30days_output = tk.Label(main_window, text = f"{date_before_30days_format}")
+date_before_30days_output.grid(row = 6, column = 2)
 
-label_result8 = Label(main_window, text = 0)
-label_result8.grid(row = 7, column = 2)
+date_before_30day_difference_output = tk.Label(main_window, text = 0)
+date_before_30day_difference_output.grid(row = 7, column = 2)
 
 main_window.mainloop()
